624d24c47e703b4aa4c5be40591c8affa68e5e14
蔡家乐 <3202787135@qq.com>
2021-06-20 23:11:20 +0800

    提交一下

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
-import 'antd/dist/antd.less'
+import {Route,Switch,Redirect} from 'react-router-dom'
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 
 export default class App extends Component {
     render() {
         return (
-            <div>
-                App
-            </div>
+            <Switch>
+                <Route path="/login" component={Login}/>
+                <Route path="/admin" component={Admin}/>
+                <Redirect to="/login"/>
+            </Switch>
         )
     }
 }
